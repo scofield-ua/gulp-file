f06d3b0d356a57cb1c3a15408984c9db6592343b
Dmitriy Scofield <dimka.tkach@gmail.com>
2016-08-01 14:41:01 +0300

    files

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp'),
+    stylus = require('gulp-stylus'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    cssmin = require('gulp-cssmin'),
+    minify = require('gulp-minify'),
+    gulpIgnore = require('gulp-ignore');
+
+gulp.task('stylus', function() {
+    return gulp.src([
+        'resources/assets/stylus/*.styl',
+    ])
+    .pipe(concat('build.styl'))
+    .pipe(stylus())
+    .pipe(gulp.dest('public/css'))
+});
+
+gulp.task('css', ['stylus'], function() {
+    return gulp.src([
+        ''
+    ])
+    .pipe(cssmin())
+    .pipe(gulpIgnore('*.min'))
+    .pipe(concat('build.css'))
+    .pipe(gulp.dest('public/css'))
+});
+
+gulp.task('watch', ['css'], function() {
+    gulp.watch('resources/assets/stylus/*.styl', ['css']);
+});
